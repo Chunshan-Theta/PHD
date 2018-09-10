650e2d3dcb881630dc1d871cfff6d9da0ec93b05
gavin <f106524018@g.ncu.edu.tw>
2018-09-10 16:12:45 +0800

    update

---
+++ PHD UI Mockup/testing/ShowSteps.js
@@ -0,0 +1,71 @@
+/* a step
+<div id="container" class="border-bottom border-black">
+    <div class="media text-muted pt-3 row">
+      <div class="col-md-4 p-3 my-1">Final Exam </div>
+      <div class="col-md-6 p-3 my-1">has two chance. you need passing the exam before graduation.</div>
+      <div class="col-md-2 p-3 my-1"><i class="fas fa-check text-green"></i></div>
+
+    </div>
+    <div class="pt-3 p-3 bg-lightgray ">
+        <li class="text-right "><i class="fas pl-3 fa-edit" title="編輯"></i></li>
+        <li>第一次測試結果 未通過，原因分數未達標準</li>
+        <li>第二次測試結果 通過，恭喜～～～～</li>
+        <li class="text-right text-gray">deadline:2022/09/22</li>
+    </div>
+</div>
+*/
+
+function newastep(name){
+  console.log(name);
+}
+function showsteps(name){
+  $("#addstep").off("click");
+  $.cookie('chosenmember', name, { expires: 1 });
+  $("#addstep").attr("onclick",'newastep(\''+name+'\');');
+  //$("#addstep").addClass("xzc");
+
+  $("#stepslistcontent").remove();
+  const m = JSON.parse($.cookie(name));
+
+  $("#stepslist").append('<div id="stepslistcontent">');
+  //console.log(m.steps);
+
+  for(var idx in m.steps){
+    addasteptodiv(m.steps[idx]);
+  }
+
+}
+function addasteptodiv(step){
+  const containername = "container_"+step.sid;
+  const containerinfo = "info_"+step.sid;
+  const containerlog = "log_"+step.sid;
+
+  $("#stepslistcontent").append('<div id="'+containername+'" class="border-bottom border-black">');
+  $("#"+containername).append('<div id="'+containerinfo+'" class="media text-muted pt-3 row likebutton" onclick="showlog(\''+containerlog+'\')">');
+  $("#"+containername).append('<div id="'+containerlog+'" class="pt-3 p-3 bg-lightgray d-none ">');
+
+  $("#"+containerinfo).append('<div class="col-md-4 p-3 my-1">'+step.title+' </div>');
+  $("#"+containerinfo).append('<div class="col-md-6 p-3 my-1">'+step.description+'</div>');
+  if(step.status == true){
+    $("#"+containerinfo).append('<div class="col-md-2 p-3 my-1"><i class="fas fa-check text-green"></i></div>');
+  }else if (step.status == false) {
+    $("#"+containerinfo).append('<div class="col-md-2 p-3 my-1"><i class="fas fa-times text-red"></i></div>');
+  }else{
+    $("#"+containerinfo).append('<div class="col-md-2 p-3 my-1"><i class="fas fa-question-circle text-gray"></i></div>');
+  }
+
+  $("#"+containerlog).append('<li class="text-right "><i class="fas pl-3 fa-edit" title="編輯"></i></li>');
+  $("#"+containerlog).append('<li>'+step.log+'</li>');
+  $("#"+containerlog).append('<li class="text-right text-gray">deadline:'+step.deadline+'2</li>');
+
+
+}
+function showlog(nameid){
+  //console.log(nameid);
+  if($("#"+nameid).hasClass("d-none")){
+    $("#"+nameid).removeClass("d-none");
+  }else{
+    $("#"+nameid).addClass("d-none");
+  }
+
+}
--- PHD UI Mockup/testing/submembers.js
+++ PHD UI Mockup/testing/submembers.js
@@ -1,4 +1,3 @@
-
 /*
 {
   "members": [
@@ -40,32 +39,45 @@ class member {
 }
 
 class step {
-  constructor(sid,group,title,deadline,log,status) {
+  constructor(sid,group,title,deadline,log,status,description) {
     this.sid = sid;
     this.group = group;
     this.title = title;
+    this.description = description;
     this.deadline = deadline;
     this.log = log;
     this.status = status;
   }
 }
 
-var step1 = new step("s1","NCU-NLT","title1","2019-12-31","None",0);
-var step2 = new step("s2","NCU-NLT","title2","2019-12-31","None",0);
-var step3 = new step("s3","NCU-NLT","title3","2019-12-31","None",0);
-var member1 = new member("m1","Tony","NCU-NLT","user",[step1,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524018\",\"聯絡方式\":\"DemoStudent1@mail.com\"}");
-var member2 = new member("m2","Gavin","NCU-NLT","user",[step1,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524019\",\"聯絡方式\":\"DemoStudent2@mail.com\"}");
-var member3 = new member("m3","Theta","NCU-NLT","user",[step1,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524020\",\"聯絡方式\":\"DemoStudent3@mail.com\"}");
 
 
-var submembers = {
+var step1_1 = new step("s4","NCU-NLT","title4","2019-12-31","4",0,"description1");
+var step1_2 = new step("s5","NCU-NLT","title5","2019-12-31","5",1,"description2");
+var step1_3 = new step("s6","NCU-NLT","title6","2019-12-31","6",null,"description3");
+var step2 = new step("s2","NCU-NLT","title2","2019-12-31","None",1,"description4");
+var step3 = new step("s3","NCU-NLT","title3","2019-12-31","None",0,"description5");
+var member1 = new member("m1","Tony","NCU-NLT","user",[step1_1,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524018\",\"聯絡方式\":\"DemoStudent1@mail.com\"}");
+var member2 = new member("m2","Gavin","NCU-NLT","user",[step1_2,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524019\",\"聯絡方式\":\"DemoStudent2@mail.com\"}");
+var member3 = new member("m3","Theta","NCU-NLT","user",[step1_3,step2,step3],false,"{\"入學年\":\"2018\",\"學號\":\"106524020\",\"聯絡方式\":\"DemoStudent3@mail.com\"}");
+
+
+var input_submembers = {
   "members": [member1,member2,member3]
 }
-console.log("hi");
-console.log(submembers["members"]);
-addmembertolist(member1);
-addmembertolist(member2);
-addmembertolist(member3);
+
+
+
+
+
+
+for(var idx in input_submembers["members"]){
+  const m = input_submembers["members"][idx];
+  $.cookie(m.name, JSON.stringify(m), { expires: 1 });
+  addmembertolist(m);
+}
+
+
 //$("#members").append(" <b>Hello world!</b>");
 /* a member
 <div class="media align-items-center text-muted pt-3">
@@ -90,11 +102,11 @@ function addmembertolist(member){
   const unitp = 'unitp_'+member.name;
   const uniticon = 'uniticon_'+member.name;
   const unitinfo = 'unitinfo_'+member.name;
-  $("#members").append('<div id="'+unitdiv+'" class="media align-items-center text-muted pt-3">');
+  $("#members").append('<div id="'+unitdiv+'" class="media align-items-center text-muted pt-3 likebutton" onclick="showsteps(\''+member.name+'\')">');
   $("#"+unitdiv).append('<i class="fas fa-user p-3 "></i>');
   $("#"+unitdiv).append('<p id="'+unitp+'" class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">');
   $("#"+unitp).append('<strong id="'+uniticon+'" class="d-block text-right">');
-  $("#"+uniticon).append('<i class="fas fa-exclamation p-1 text-red "></i>');
+  $("#"+uniticon).append('<i class="fas fa-exclamation p-1 text-red d-none"></i>');
   $("#"+uniticon).append('<i class="fas fa-plus p-1"></i>');
   $("#"+uniticon).append('<i class="fas fa-times p-1"></i>');
   $("#"+unitp).append('<strong class="d-block text-black">'+member.name+'</strong>');
